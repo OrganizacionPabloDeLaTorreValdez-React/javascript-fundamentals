60a87965ac51f1aecc934534a6f562cd467acc84
rolandoandre123 <rbardalespriale@gmail.com>
2023-02-19 16:46:26 -0500

    add : dataset y elementById

--- js/23_selectores_dom.js
+++ js/23_selectores_dom.js
@@ -1,19 +1,29 @@
 
 // Selectores del DOM
 
+// Obteniendo un elemento u elementos HTML
+
 // const heading = document.querySelector('h2')
 // const heading = document.querySelector('.DOM h2')
 // const heading = document.querySelector('#heading')
-const heading = document.querySelector('.heading')
+let heading = document.querySelector('.heading')
 console.log(heading.textContent) // Probando el DOM
 console.log(heading.tagName) // H2
 console.log(heading.classList)
-console.log(heading.id)
+console.log(heading.id) // heading
 
 const enlaces = document.querySelectorAll('.navegacion a')
 enlaces.forEach(en => {
   console.log(en.textContent)
 })
+
+// obtener un elemento por su id
+heading = document.getElementById("heading")
+console.log(heading.textContent) // Probando el DOM
+// obteniendo información del dataset, data-**
+console.log(heading.dataset.info) // info1
+// obteniendo información de los atributos
+console.log(heading.attributes.info.value) // info2
 
 
 
